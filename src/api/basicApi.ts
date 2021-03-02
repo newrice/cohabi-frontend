c2aab00bc1dc62cbe8a5d1d5913f0e62e4107971
岩永尚己 <naoki.vienna@gmail.com>
2021-03-03 00:35:19 +0900

    [fix] Remove fetch user api call function

--- src/api/basicApi.ts
+++ src/api/basicApi.ts
@@ -1,6 +1,6 @@
 import { getData, BASE_BACKEND } from "./api-base";
 import settings from "../settings";
-import { IApiResponseBase, ICategory, IUser, QueryParams } from "../types";
+import { IApiResponseBase, ICategory, QueryParams } from "../types";
 
 export const fetchDataBase = <T>(
   url: string,
@@ -29,12 +29,4 @@ export const fetchCategories = (
   fetchGroupedDataBase<ICategory[] | undefined>(
     groupId,
     `${BASE_BACKEND}${settings.url.categories}`,
-  );
-
-export const fetchUsers = (
-  groupId: string,
-): Promise<IApiResponseBase<IUser[] | undefined>> =>
-  fetchGroupedDataBase<IUser[] | undefined>(
-    groupId,
-    `${BASE_BACKEND}${settings.url.users}`,
   );
